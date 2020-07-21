0b113035beb24f09cd6142d416e80a2fdadacd0e
Zong Hao Ben Chang <s10185167@connect.np.edu.sg>
2020-07-21 14:29:22 +0800

    updated intent

---
+++ app/src/main/java/sg/edu/np/madteam6/assignment/GongCha.java
@@ -0,0 +1,109 @@
+package sg.edu.np.madteam6.assignment;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class GongCha extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    final String TAG = "bbt app";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gong_cha);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        //creating bottom navigation bar
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        //setting page as home
+        bottomNavigationView.setSelectedItemId(R.id.location);
+        //setting up listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                //creating intents to go to another page
+                switch(menuItem.getItemId()){
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext()
+                                , MainMenu.class));
+                        overridePendingTransition(0,0);
+                        Log.v(TAG,"going to home");
+                        return true;
+                    case R.id.menu:
+                        startActivity(new Intent(getApplicationContext()
+                                , Menu.class));
+                        overridePendingTransition(0,0);
+                        Log.v(TAG,"going to Menu");
+                        return true;
+
+                    case R.id.location:
+
+                        return true;
+
+
+                    case R.id.tracker:
+                        startActivity(new Intent(getApplicationContext()
+                                , Tracker.class));
+                        overridePendingTransition(0,0);
+                        Log.v(TAG,"going to tracker");
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
+
+    }
+
+
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.animateCamera( CameraUpdateFactory.zoomTo( 12) );
+    }
+
+    public void sendMessageliho(View view) {
+        Intent intent = new Intent(GongCha.this, Liho.class);
+        startActivity(intent);
+    }
+
+
+    public void sendMessagekoi(View view) {
+        Intent intent = new Intent(GongCha.this, Koi.class);
+        startActivity(intent);
+    }
+}
